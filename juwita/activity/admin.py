a031575468d9b08ce41804e93d2416d522de2a96
Dan <danielgao2766@gmail.com>
2026-03-21 15:58:38 -0400

    added ET Time display

--- juwita/activity/admin.py
+++ juwita/activity/admin.py
@@ -1,22 +1,28 @@
 from django.contrib import admin
 from .models import Log
-from django.utils.timezone import localtime
-import pytz
+from zoneinfo import ZoneInfo
+from django.utils.timezone import is_aware, make_aware
+from datetime import timezone  # for UTC
 
-
-EASTERN_TZ = pytz.timezone('US/Eastern')
+EASTERN_TZ = ZoneInfo("America/New_York")
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
-    list_display = ('action', 'timestamp_local', 'metadata')
+    list_display = ('action', 'timestamp_eastern', 'metadata')
     list_filter = ('timestamp',)
     search_fields = ('action',)
 
-    def timestamp_local(self, obj):
-        # Convert UTC to your desired timezone
-        local_time = obj.timestamp.astimezone(EASTERN_TZ)
-        # If you prefer Eastern Time, use EASTERN_TZ
-        return local_time.strftime('%Y-%m-%d %H:%M:%S %Z')
+    def timestamp_eastern(self, obj):
+        timestamp = obj.timestamp
 
-    timestamp_local.admin_order_field = 'timestamp'  # allows sorting
-    timestamp_local.short_description = 'Timestamp (Pacific)'
+        # Make naive datetime aware as UTC
+        if not is_aware(timestamp):
+            timestamp = make_aware(timestamp, timezone.utc)
+
+        # Convert to Eastern Time
+        eastern_time = timestamp.astimezone(EASTERN_TZ)
+        return eastern_time.strftime('%Y-%m-%d %H:%M:%S %Z')
+
+    # sort by original UTC
+    timestamp_eastern.admin_order_field = 'timestamp'  
+    timestamp_eastern.short_description = 'Timestamp (Eastern)'
