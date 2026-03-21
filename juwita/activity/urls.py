7cd2b5f5ccfc439e4d6e15f2fd57f13129523342
jacerniello <75341926+jacerniello@users.noreply.github.com>
2026-03-21 16:59:24 -0400

    Allow URLs without trailing slash

--- juwita/activity/urls.py
+++ juwita/activity/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/log/', views.log_action, name='log_action'),
+    path('api/log', views.log_action),
     path('api/ping/', views.ping, name='ping'),
+    path('api/ping', views.ping),
     path('api/meetings/', views.meetings, name='meetings'),
+    path('api/meetings', views.meetings),
 ]
